5e258db2229c12f25978544289eb9c9b2c13ec16
Lukasz Mowinski <lmowinski@deloittece.com>
2022-12-19 22:20:59 +0100

    feat: controller stud

---
+++ src/main/java/pl/put/poznan/texttransformer/controller/TextTransformerController.java
@@ -0,0 +1,41 @@
+package pl.put.poznan.texttransformer.controller;
+
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+import pl.put.poznan.texttransformer.service.TextTransformerService;
+
+@RestController
+@RequestMapping("/{text}")
+@AllArgsConstructor
+public class TextTransformerController {
+
+    private static final Logger logger = LoggerFactory.getLogger(TextTransformerController.class);
+    private TextTransformerService textTransformerService;
+
+    @GetMapping(produces = "application/json")
+    public String get(@PathVariable String text,
+                      @RequestParam(value="transforms", defaultValue="upper,escape") String transforms) {
+
+        // log the parameters
+        logger.debug(text);
+        logger.debug(transforms);
+
+        // perform the transformation, you should run your logic here, below is just a silly example
+        return textTransformerService.transform(transforms, text);
+    }
+
+    @PostMapping(produces = "application/json")
+    public String post(@PathVariable String text,
+                       @RequestBody String transforms) {
+
+        // log the parameters
+        logger.debug(text);
+        logger.debug(transforms);
+
+        // perform the transformation, you should run your logic here, below is just a silly example
+        return textTransformerService.transform(transforms, text);
+    }
+
+}
