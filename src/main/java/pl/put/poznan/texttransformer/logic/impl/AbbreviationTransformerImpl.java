2910262902a93170690fe9fec672ece1632189d2
Lukasz Mowinski <lmowinski@deloittece.com>
2022-12-20 01:18:54 +0100

    feat: added interfaces studs

---
+++ src/main/java/pl/put/poznan/texttransformer/logic/impl/AbbreviationTransformerImpl.java
@@ -0,0 +1,6 @@
+package pl.put.poznan.texttransformer.logic.impl;
+
+import pl.put.poznan.texttransformer.logic.AbbreviationTransformer;
+
+public class AbbreviationTransformerImpl implements AbbreviationTransformer {
+}
---
+++ src/main/java/pl/put/poznan/texttransformer/logic/impl/NumberTransformerImpl.java
@@ -0,0 +1,6 @@
+package pl.put.poznan.texttransformer.logic.impl;
+
+import pl.put.poznan.texttransformer.logic.NumberTransformer;
+
+public class NumberTransformerImpl implements NumberTransformer {
+}
